f4499c5ffe72cbeb7a2634899dd5c1e346613ced
Bhanukakumara <bhanukarasika@gmail.com>
2025-03-28 16:22:43 +0530

    exam log

--- src/main/java/edu/bks/controller/ExamController.java
+++ src/main/java/edu/bks/controller/ExamController.java
@@ -1,6 +1,9 @@
 package edu.bks.controller;
 
 import edu.bks.dto.Exam;
+import edu.bks.service.ExamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/exam")
+@RequiredArgsConstructor
 public class ExamController {
+    final ExamService examService;
     @PostMapping("/create-exam")
     public void createExam(@RequestBody Exam exam){
-
+        examService.createExam(exam);
     }
 }
--- src/main/java/edu/bks/service/impl/ExamServiceImpl.java
+++ src/main/java/edu/bks/service/impl/ExamServiceImpl.java
@@ -2,13 +2,17 @@ package edu.bks.service.impl;
 
 import edu.bks.dto.Exam;
 import edu.bks.service.ExamService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@Slf4j
 public class ExamServiceImpl implements ExamService {
     @Override
     public void createExam(Exam exam) {
-
+        log.info(exam.toString());
     }
 
     @Override
